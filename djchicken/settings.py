a41f37812fe05c4607e4fbae266c2723b243f31d
Tran Khang <58170807+rocketvn@users.noreply.github.com>
2022-10-30 15:04:27 +0700

    fix setting

--- djchicken/settings.py
+++ djchicken/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!=xzpfjxk9w$gl&p)2x6=*+n&7$6u6(6)3+19nqfbturt$t7$o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
