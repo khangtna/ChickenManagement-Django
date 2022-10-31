6a20a37af6634ce700228d14a7f1821756abba89
Tran Khang <58170807+rocketvn@users.noreply.github.com>
2022-10-31 16:30:32 +0700

    fix db

--- djchicken/settings.py
+++ djchicken/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!=xzpfjxk9w$gl&p)2x6=*+n&7$6u6(6)3+19nqfbturt$t7$o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -150,3 +152,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 
 ]
+
+django_heroku.settings(locals())
